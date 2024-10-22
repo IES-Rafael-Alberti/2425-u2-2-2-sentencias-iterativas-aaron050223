dffa936b62cafd2be59a35e50c244ad81084e106
aaron050223 <agalcan1410@g.educaand.es>
2024-10-22 19:55:10 +0200

    A2

--- src/Ejercicios/ej02.py
+++ src/Ejercicios/ej02.py
@@ -3,7 +3,11 @@ Escribir un programa que pregunte al usuario su edad y
 muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
 '''
 
+## Pedimos la edad
+
 edad = int(input("introduzca su edad: "))
+
+## En esta funcion se entregara como la actividad indica
 
 def main():
     for numero in range(1,edad +1):
